59d123d385328e8b2054a934500e3a7c4335d13b
eashwar <eashwarkumard@gmail.com>
2015-08-26 23:37:20 -0400

    assignment 1 files

---
+++ test1.rb
@@ -0,0 +1,11 @@
+
+
+
+x= "ruby     is a     pure    object oriented    programming"
+ puts x.split.join(" ")
+
+
+
+ y=" a b   c   d e f  g"
+ puts y.split.join(" ")
+
---
+++ test2.rb
@@ -0,0 +1,26 @@
+h1={a: 1, b: 2, c: 3, d: 4}
+#puts h1.values
+h2={a: 1, b: "test", c: 3, d: 4}
+#puts h2.values
+
+h3={a: 1, b: 2, c: {x: 10, y: 20}, d: 4}
+#puts h3.values
+
+h4={a: 1, b: 2, c: {x: {state: 'TX', country: 'USA'}, y: 20}, d: 4, e: {f: {g: {k: 'end'}}}}
+#puts h4.values
+
+##here i am defining the method "recur", so when value is again hash it has to go to the method untill to get a value 
+##if "x" is a value it will be printed in else condition , so we will only values
+def recur(h)
+	h.values.each do |x|
+		if x.class==Hash
+			recur(x)
+		else 
+			puts x
+		end	
+	end
+
+end
+
+recur(h4)
+
